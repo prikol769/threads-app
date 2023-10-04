b0a332b8fdd1db3d6f024abb75d0c3421aaccf67
prikol769 <prikol769@gmail.com>
2023-10-04 15:27:47 -0500

    added auth with clerk

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,9 @@
+import {UserButton} from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <main>
-      Threads
-    </main>
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
   )
 }
