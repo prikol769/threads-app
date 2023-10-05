938c07f48b17ab11efb437ad09282cab38173387
prikol769 <prikol769@gmail.com>
2023-10-05 11:24:15 -0500

    added Home page

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import {SignedIn, SignOutButton, OrganizationSwitcher} from "@clerk/nextjs";
+import {dark} from "@clerk/themes";
 
 const Topbar = () => {
     return (
@@ -36,6 +37,7 @@ const Topbar = () => {
 
                 <OrganizationSwitcher
                     appearance={{
+                        baseTheme: dark,
                         elements: {
                             organizationSwitcherTrigger: "py-2 px-4"
                         }
